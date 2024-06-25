023953b9ceaa5667b2807b00b71112090db2bd8f
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-06-25 12:18:35 +0900

    refactor: fibonacci (#7)
    1. 반복문 구현
    2. 메모제네이션 구현

--- dp/fibonacci/memoization.go
+++ dp/fibonacci/memoization.go
@@ -8,22 +8,28 @@ import "fmt"
 func main() {
 	n := 10
 
-	memo := make(map[int]int)
-	fmt.Printf("Fibonacci(%d) = %d\n", n, fibonacciMemoization(n, memo))
+	memoization := make(map[int]int)
+	fmt.Printf("Fibonacci(%d) = %d\n", n, fibonacciMemoization(n, memoization))
 }
 
-func fibonacciMemoization(n int, memo map[int]int) int {
+func fibonacciMemoization(n int, memoization map[int]int) int {
 	if n <= 1 {
 		return n
 	}
 
-	if _, exists := memo[n]; !exists {
-		memo[n] = fibonacciMemoization(n-1, memo) + fibonacciMemoization(n-2, memo)
+	if value, exists := memoization[n]; exists {
+		return value
 	}
-	return memo[n]
+
+	memoization[n] = fibonacciMemoization(n-1, memoization) + fibonacciMemoization(n-2, memoization)
+	return memoization[n]
 }
 
 /*
 [메모제이션 방식]
 재귀 호출 시 이미 계산된 값을 저장하여 중복 계산을 피한다. 공간과 시간 복잡도 모두 효율적.
+
+1. memoization 변수는 공유 자원이다.
+
+2. comma ok 패턴을 사용하여 키가 존재하는지 확인, 키가 존재하면 해당 값을 반환하고, 존재하지 않으면 피보나치 수를 계산하여 memoization 맵에 저장한 후 반환.
 */
