79342f95759c2b618845404fa1dd1a63c491cd93
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-07-19 09:01:50 +0900

    feat: brute force
    1. brute_force/practice_test

--- brute_force/practice.go
+++ brute_force/practice.go
@@ -1,5 +1,5 @@
 // TODO: 모의고사
-// TODO:
+// TODO: 수포자들이 정답을 맞추는 패턴을 정의하고, 각 수포자의 패턴을 반복하여 주어진 답안과 비교한 후 가장 많은 정답을 맞춘 수포자를 찾는다.
 // https://school.programmers.co.kr/learn/courses/30/lessons/42840
 
 package main
@@ -8,45 +8,61 @@ import (
 	"fmt"
 )
 
-var supoza1 = []int{1, 2, 3, 4, 5}
-var supoza2 = []int{2, 1, 2, 3, 2, 4, 2, 5}
-var supoza3 = []int{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+// 수포자들의 답안 패턴을 명확한 변수명으로 정의
+var studentOnePattern = []int{1, 2, 3, 4, 5}
+var studentTwoPattern = []int{2, 1, 2, 3, 2, 4, 2, 5}
+var studentThreePattern = []int{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
 
-func solution(answers []int) []int {
-	scores := make([]int, 3)
+func findTopScorers(answers []int) []int {
+	scores := calculateScores(answers)
+	return identifyTopScorers(scores)
+}
+
+// 각 수포자의 점수를 계산
+func calculateScores(answers []int) []int {
+	scores := []int{0, 0, 0}
 
 	for i, answer := range answers {
-		if answer == supoza1[i%len(supoza1)] {
+		if answer == studentOnePattern[i%len(studentOnePattern)] {
 			scores[0]++
 		}
-		if answer == supoza2[i%len(supoza2)] {
+		if answer == studentTwoPattern[i%len(studentTwoPattern)] {
 			scores[1]++
 		}
-		if answer == supoza3[i%len(supoza3)] {
+		if answer == studentThreePattern[i%len(studentThreePattern)] {
 			scores[2]++
 		}
 	}
 
-	maxScore := max(scores[0], max(scores[1], scores[2]))
+	return scores
+}
 
-	var result []int
+// 가장 많은 정답을 맞춘 수포자를 찾음
+func identifyTopScorers(scores []int) []int {
+	maxScore := max(scores...)
+	var topScorers []int
+
 	for i, score := range scores {
 		if score == maxScore {
-			result = append(result, i+1)
+			topScorers = append(topScorers, i+1)
 		}
 	}
 
-	return result
+	return topScorers
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
+// 가변 인자를 받아 최대값을 반환
+func max(nums ...int) int {
+	maxNum := nums[0]
+	for _, num := range nums {
+		if num > maxNum {
+			maxNum = num
+		}
 	}
-	return b
+	return maxNum
 }
 
 func main() {
-	fmt.Println(solution([]int{1, 2, 3, 4, 5})) // Output: [1]
-	fmt.Println(solution([]int{1, 3, 2, 4, 2})) // Output: [1,2,3]
+	fmt.Println(findTopScorers([]int{1, 2, 3, 4, 5})) // Output: [1]
+	fmt.Println(findTopScorers([]int{1, 3, 2, 4, 2})) // Output: [1,2,3]
 }
